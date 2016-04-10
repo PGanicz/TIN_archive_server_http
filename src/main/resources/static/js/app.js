ebeccd215d3cf83c7f87e922bd587cf1d82b8ba2
Piotr Ganicz <piotr.ganicz@gmail.com>
2016-04-10 23:19:46 +0200

    Remove example login page directory (/resources/static)

--- src/main/resources/static/js/app.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by piotr on 31.03.16.
- */
-var springGame = angular.module('springGame', ['springGame.controllers', 
-                                               'springGame.services']);
