f6c93c9ae0bb19d22cfa1ca439824d64758f41d1
RNowak4 <Radek095@gmail.com>
2016-04-24 19:21:38 +0200

    Fixed register controller

--- src/main/java/com/websocket/game/web/UserController.java
+++ src/main/java/com/websocket/game/web/UserController.java
@@ -3,37 +3,28 @@ package com.websocket.game.web;
 import com.websocket.game.DAO.UserRepository;
 import com.websocket.game.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
-/**
- * Created by piotr on 10.04.16.
- */
 @RestController
-@RequestMapping(value="/user")
-public class UserController
-{
+@RequestMapping(value = "/user")
+public class UserController {
 
     @Autowired
     public UserRepository userRepository;
+
     /*
      *  This method provides the name of current user
      */
     @RequestMapping(method = RequestMethod.GET)
-    public Principal user(Principal user)
-    {
+    public Principal user(Principal user) {
         return user;
     }
 
-    @RequestMapping(value="/register",method = RequestMethod.POST)
-    public void register(@ModelAttribute("user") User user)
-    {
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    @ResponseBody
+    public void register(@RequestBody User user) {
         userRepository.save(user);
     }
-
-
 }
